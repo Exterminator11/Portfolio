65437a1e4560411cdf89a7eb8ce77956351eafa1
Rachit Das <rachit1031@gmail.com>
2025-06-15 19:56:20 +0530

    fix

--- portfolio/src/app/layout.js
+++ portfolio/src/app/layout.js
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Rachit`&apos`s Portfolio",
+  title: "Rachits Portfolio",
   description: "Rachit Das - Machine Learning Engineer",
 };
 
